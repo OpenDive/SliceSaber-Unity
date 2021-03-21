91cd0975c704b1478e1f72fdc8884aa43b736db6
Xiangxu Lin <cricel.design@gmail.com>
2021-03-21 00:25:53 -0400

    login ui

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateSpeed.x * Time.deltaTime, rotateSpeed.y * Time.deltaTime, rotateSpeed.z * Time.deltaTime);
+    }
+}
